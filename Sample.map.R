5af36ab3e2d1d4e6b75d02d9e0603c73d4165c02
Lori R. Shapiro <lori.r.shapiro@gmail.com>
2019-05-21 12:02:27 -0400

    updated scripts

--- Sample.map.R
+++ Sample.map.R
@@ -11,9 +11,13 @@ require(mapdata); require(maptools)
 
 theme_set(theme_bw())
 
-setwd("/Users/lorishapiro/Dropbox/Matrix_forsharing_16S/Bee-beetle-only-beta")
+setwd("~/Dropbox/Matrix_forsharing_16S/Bee-beetle-only-beta")
 
 samples <- read.table("samples.txt", sep="\t", header=TRUE, fileEncoding="UTF-16")
+str(samples)
+samples <- as_tibble(samples) 
+
+#samples <- filter(samples, Type != "Peponapis pruinosa")## Subset data
 
 usa <- map_data("usa")
 canada <- map_data("worldHires", "Canada")
@@ -23,6 +27,8 @@ NAmap <- ggplot() + geom_polygon(data = usa,
                                  aes(x=long, y = lat, group = group), 
                                  fill = "grey88", 
                                  color="black") +
+  xlab("Longitude") +
+  ylab("Latitude") +
   ggtitle("Insect Collection Locations") +
   geom_polygon(data = canada, aes(x=long, y = lat, group = group), 
                fill = "grey88", color="black") + 
@@ -30,12 +36,19 @@ NAmap <- ggplot() + geom_polygon(data = usa,
                fill = "grey88", color="black") +
   coord_fixed(xlim = c(-125, -65),  ylim = c(18, 50), ratio = 1.3) +
   geom_point(data=samples, aes(x=Longitude, y=Latitude),
-             fill=samples$Color, color = "black", shape=21, size=5.0) #+
-  theme(line = element_blank(),
-        text = element_blank(), 
-        rect = element_blank()) 
-NAmap 
-NAmap + labs(colour = "Type") ## Can't figure out how to get the legend to print
+             fill=samples$Color, color = "black", shape=21, size=samples$sampleSize) +
+  theme(plot.title = element_text(lineheight=.8, face="bold", hjust=0.5, size=16),
+            axis.text.x  = element_text(face="bold", color = "black", hjust=0.5, size=10),
+            axis.title.y = element_text(face="bold", color = "black", size=12),
+            axis.title.x = element_text(face="bold", color = "black", size=12),
+            axis.text.y  = element_text(face="bold", color = "black", vjust=0.5, size=10),
+            legend.title = element_text(size=12, face="bold", hjust = 0.5),
+            legend.text = element_text(size = 10, face = "bold") +
+theme(legend.position="top"))
+
+NAmap  ## Can't figure out how to get the legend to print
+
+ggsave("sample.map.pdf", height=6, width=6)
 
 dev.off()
 
@@ -48,3 +61,34 @@ colors()[grep("sky",colors())]
 col2rgb("orange")
 col2rgb("darkolivegreen")
 col2rgb("darkolivegreen2")
+
+-------------------------------------------------------------------
+  ## Map for grant
+  NAmap <- ggplot() + geom_polygon(data = usa, 
+                                   aes(x=long, y = lat, group = group), 
+                                   fill = "grey88", 
+                                   color="black") +
+  xlab("Longitude") +
+  ylab("Latitude") +
+  ggtitle("Erwinia tracheiphila epidemic region") +
+  geom_polygon(data = canada, aes(x=long, y = lat, group = group), 
+               fill = "grey88", color="black") + 
+  geom_polygon(data = mexico, aes(x=long, y = lat, group = group), 
+               fill = "grey88", color="black") +
+  coord_fixed(xlim = c(-125, -65),  ylim = c(18, 50), ratio = 1.3) #+
+#  geom_point(data=samples, aes(x=Longitude, y=Latitude),
+#             fill=samples$Color, color = "black", shape=21, size=samples$sampleSize) +
+NAmap + theme(plot.title = element_text(lineheight=.8, face="bold", hjust=0.5, size=12),
+        axis.text.x  = element_text(face="bold", color = "black", hjust=0.5, size=10),
+        axis.title.y = element_text(face="bold", color = "black", size=10),
+        axis.title.x = element_text(face="bold", color = "black", size=10),
+        axis.text.y  = element_text(face="bold", color = "black", vjust=0.5, size=10),
+        legend.title = element_text(size=12, face="bold", hjust = 0.5),
+        legend.text = element_text(size = 10, face = "bold") +
+          theme(legend.position="top"))
+
+NAmap  ## Can't figure out how to get the legend to print
+
+ggsave("sample.map.pdf", height=6, width=6)
+
+dev.off()
